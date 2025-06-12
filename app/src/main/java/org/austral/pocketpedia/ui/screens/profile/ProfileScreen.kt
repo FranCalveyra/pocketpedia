da8df64432629d0c81192cbdc2da02ea4283a759
FranCalveyra <franciscocalveyra24@gmail.com>
2025-06-12 13:41:34 -0300

    Updated icon size and profile screen text alignment

--- app/src/main/java/org/austral/pocketpedia/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/org/austral/pocketpedia/ui/screens/profile/ProfileScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -71,6 +72,7 @@ fun ProfileScreen() {
                     stringResource(R.string.welcome_trainer, user?.displayName ?: ""),
                     style = typography.titleLarge,
                     overflow = TextOverflow.Clip,
+                    textAlign = TextAlign.Center,
                     maxLines = 2
                 )
                 ProfileBody(
